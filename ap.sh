93ecb52206ddbf3371cb8d49e8889eddb0b77015
ITACHI UCHIHA <143554070+ITACHI-143@users.noreply.github.com>
2023-09-05 04:39:58 +0600

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #Written by ARIYAN-XD
-while true; do
+num_repeats=5  # Change this to the desired number of repetitions
+
+for ((i = 1; i <= num_repeats; i++)); do
     echo "Turning on airplane mode"
     settings put global airplane_mode_on 1
     am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true
+    
     sleep 3
+
     echo "Turning off airplane mode"
     settings put global airplane_mode_on 0
     am broadcast -a android.intent.action.AIRPLANE_MODE --ez state false
+
     echo "Waiting for 2 minutes..."
     sleep 120
+
+    echo "Iteration $i completed."
 done
